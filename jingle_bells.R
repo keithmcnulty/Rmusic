66922081bed5542d66706719cc3e801c4ab6e1d3
keithmcnulty <keith.mcnulty@gmail.com>
2017-12-18 12:32:27 +0000

    Create jingle_bells.R

---
+++ jingle_bells.R
@@ -0,0 +1,34 @@
+library("dplyr")
+library("audio")
+notes <- c(a = 0, b = 2, c = 3, d = 5, e = 7, f = 8, g = 10)
+pitch <- "e e e e e e e g c d e"
+duration <- c(1, 1, 2, 1, 1, 2, 1, 1, 1.5, 0.5, 4)
+bday <- data_frame(pitch = strsplit(pitch, " ")[[1]],
+                   duration = duration)
+
+bday <-
+  bday %>%
+  mutate(octave = substring(pitch, nchar(pitch)) %>%
+  {suppressWarnings(as.numeric(.))} %>%
+    ifelse(is.na(.), 4, .),
+  note = notes[substr(pitch, 1, 1)],
+  note = note + grepl("#", pitch) -
+    grepl("b", pitch) + octave * 12 +
+    12 * (note < 3),
+  freq = 2 ^ ((note - 60) / 12) * 440)
+
+tempo <- 120
+sample_rate <- 44100
+
+make_sine <- function(freq, duration) {
+  wave <- sin(seq(0, duration / tempo * 60, 1 / sample_rate) *
+                freq * 2 * pi)
+  fade <- seq(0, 1, 50 / sample_rate)
+  wave * c(fade, rep(1, length(wave) - 2 * length(fade)), rev(fade))
+}
+
+bday_wave <-
+  mapply(make_sine, bday$freq, bday$duration) %>%
+  do.call("c", .)
+
+play(bday_wave)
